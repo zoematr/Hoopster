94cff20fc60b4d1dae2055cbb162fe13bcc8c20f
SchulzKilian <schulz.kilian@outlook.de>
2023-05-15 22:02:09 +0200

    look at the processVideo functiongit add . thats where the magic is gonna happen

--- lib/screens/recording_screen2.dart
+++ lib/screens/recording_screen2.dart
@@ -34,11 +34,17 @@ class _CameraAppState extends State<CameraApp> {
   _CameraAppState(/*this.camera_*/) {
     //initState();
   }
+  void processVideo(String videoPath) {
+    //this badboy is gonna handle our video editing
+    print('Video path: $videoPath');
+  }
+
   @override
   void initState() {
     super.initState();
     controller = CameraController(cameras[0], ResolutionPreset.max);
     controller.lockCaptureOrientation(DeviceOrientation.landscapeLeft);
+
     _initializeControllerFuture = controller.initialize().then((_) {
       if (!mounted) {
         return;
@@ -65,6 +71,9 @@ class _CameraAppState extends State<CameraApp> {
         setState(() {
           _videoPath = path as String;
         });
+
+        processVideo(
+            _videoPath); // Pass the video path to the processing function
       } else {
         await _initializeControllerFuture;
         final now = DateTime.now();
